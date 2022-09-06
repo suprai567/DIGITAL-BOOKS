4255dffa65b9cb178fec8e86ebe3b7c9bcdd0a7f
suprai567 <suprai567@gmail.com>
2022-09-07 01:01:56 +0530

    initial commit

---
+++ digitalbook/src/test/java/com/digitalbooks/digitalbook/service/TestAuthorService.java
@@ -0,0 +1,92 @@
+package com.digitalbooks.digitalbook.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.digitalbooks.digitalbook.entity.Author;
+import com.digitalbooks.digitalbook.entity.Book;
+import com.digitalbooks.digitalbook.repository.AuthorRepository;
+import com.digitalbooks.digitalbook.repository.BookSearchRepository;
+
+@ExtendWith(MockitoExtension.class)
+class TestAuthorService {
+	@InjectMocks
+	BookSearchService bookSearchService;
+
+	@Mock
+	BookSearchRepository bookRepository;
+	@Mock
+	AuthorRepository authorRepository;
+
+	@Test
+	void testSave() {
+		Book books = new Book();
+		books.setId(1);
+		books.setTitle("Bhagat");
+		books.setAuthor("Chetan");
+		books.setLogo("2 States");
+		books.setCategory("comic");
+		books.setPrice(134);
+		books.setPublisher("Rupa Publication");
+		books.setPublisedDate("2022-09-08");
+		when(bookSearchService.save(books)).thenReturn(books);// mock
+		bookSearchService.save(books);
+		assertEquals(1, books.getId());
+	}
+
+	@Test
+	public void testGetBooks() throws Exception {
+		List<Book> book = new ArrayList<>();
+		Book books = new Book();
+		books.setId(1);
+		books.setTitle("Bhagat");
+		books.setAuthor("Chetan");
+		books.setLogo("2 States");
+		books.setCategory("comic");
+		books.setPrice(134);
+		books.setPublisher("Rupa Publication");
+		books.setPublisedDate("2022-09-08");
+		book.add(books);
+		when(bookRepository.findAll()).thenReturn(book);
+		Iterable<Book> getbooks = bookSearchService.getBook();
+		assertEquals(book, getbooks);
+
+	}
+
+	@Test
+	public void testGetAuthors() throws Exception {
+		List<Author> author = new ArrayList<>();
+		Author authors = new Author();
+		authors.setId(1);
+		authors.setAge(40);
+		authors.setEmail("book123@gmail.com");
+		authors.setName("Prince");
+		author.add(authors);
+		when(authorRepository.findAll()).thenReturn(author);
+		Iterable<Author> getAuthor = bookSearchService.getAuthor();
+		assertEquals(author, getAuthor);
+	}
+
+	@Test
+	void testSaveAuthor() {
+		Author authors = new Author();
+		authors.setId(1);
+		authors.setAge(40);
+		authors.setEmail("book123@gmail.com");
+		authors.setName("Prince");
+		when(authorRepository.save(authors)).thenReturn(authors);// mock
+		bookSearchService.saveAuthor(authors);
+		assertEquals(1, authors.getId());
+	}
+}
